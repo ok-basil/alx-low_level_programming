b49d9042c13d7b1ef4aadee7017e4e5e5a32be70
Basil Okache <basilokache@gmail.com>
2022-09-08 12:05:35 -0700

    quote c betty coding style corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program execution begins here 
+ * main - program execution begins here
  * Description: this is the main function
  * Return: an integer to signify success or failure
  */
@@ -9,5 +9,5 @@ int main(void)
 {
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	59, 1, stderr);
-	return(1);
+	return (1);
 }
