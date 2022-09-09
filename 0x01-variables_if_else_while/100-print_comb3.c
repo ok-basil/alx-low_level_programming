5aebcd3c52c09b7a2a164418adf3fb8a79cb9077
Basil Okache <basilokache@gmail.com>
2022-09-09 16:06:24 -0700

    betty coding

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i= '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j= '0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			if (!(j <= i))
 			{
