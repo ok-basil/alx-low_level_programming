6593f2312ddd0546723a43ea8ecbca64a425d6bb
Basil Okache <basilokache@gmail.com>
2022-09-13 13:20:42 -0700

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,7 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-			
+
 				if (j != n)
 				{
 					product = i * j;
@@ -37,5 +37,5 @@ void print_times_table(int n)
 		}
 
 	}
-	
+
 }
