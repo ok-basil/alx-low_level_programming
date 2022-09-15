0a24199d162fe09698ce55a8e272fad19518b519
Okache "Al" Basil <69770152+ok-basil@users.noreply.github.com>
2022-09-15 17:37:17 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,21 +7,25 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else
 	{
 		largest = b;
 	}
-	else
+
+	if (c > largest)
 	{
 		largest = c;
 	}
+
 	return (largest);
+
 }
