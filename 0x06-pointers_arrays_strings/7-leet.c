e97b8d0c4873df0e11a25a6434aa4fa254ddaf2e
Okache "Al" Basil <69770152+ok-basil@users.noreply.github.com>
2022-09-22 17:18:10 +0100

    letter's stuff, finally!!

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+/**
+ * leet - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char *leet(char *str)
+{
+	int letter, i = 0;
+
+	while (str[i] != '\0')
+	{
+		letter = str[i];
+		if (letter == 'A' || letter == 'a')
+			str[i] = '4';
+
+		if (letter == 'e' || letter == 'E')
+			str[i] = '3';
+
+		if (letter == 'o' || letter == 'O')
+			str[i] = '0';
+
+		if (letter == 't' || letter == 'T')
+			str[i] = '7';
+
+		if (letter == 'l' || letter == 'L')
+			str[i] = '1';
+		i++;
+	}
+	return (str)
+}
