0b7d8a86025fca7c868fc972d3ab087eeff4e2a1
Okache "Al" Basil <69770152+ok-basil@users.noreply.github.com>
2022-09-22 17:15:41 +0100

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ * string_toupper - multiplies two digits
+ * @str: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char *string_toupper(char *str)
+{
+	int i = 0, letter;
+
+	while (str[i] != '\0')
+	{
+		letter = str[i];
+		if (letter >= 'a' && letter <= 'z')
+			str[i] = letter - 32;
+		i++;
+	}
+	return (str);
+}
